1052ab2754cf8bedb6681ffae7658faf72db6ed1
[your <alessandrofigo24@gmail.com>
2022-03-27 19:52:43 -0400

    quick update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    // fontFamily: {
+    //   sans: ['"Inter"', 'sans-serif']
+    // },
     extend: {},
   },
   plugins: [],
