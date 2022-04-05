5fe45c1c48f4ffb9a4ceef02d90a38807fa2acfc
[your <alessandrofigo24@gmail.com>
2022-04-05 16:13:16 -0400

    added animation and set up about

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -7,13 +7,13 @@ const InfoCard = () => {
         <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2 font-mono">mku.eth</div>
             <p className="text-gray-700 text-base font-mono">
-            is actively learning web3
+            <strong className="text-green-700">ACTIVE NOW:</strong> Learning web3
             </p>
         </div>
         <div className="px-6 pt-2 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#ai</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#crypto</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#web3</span>
+            <span className="inline-block bg-gray-200 cursor-pointer hover:bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#ai</span>
+            <span className="inline-block bg-gray-200 cursor-pointer hover:bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#crypto</span>
+            <span className="inline-block bg-gray-200 cursor-pointer hover:bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#web3</span>
         </div>
     </div>
   )
